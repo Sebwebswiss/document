87af8173acc7b05cdc0e716d176542ccf3a0a409
sebwebswss <84191341+Sebwebswiss@users.noreply.github.com>
2024-02-19 09:26:55 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -4,17 +4,31 @@ document.addEventListener('DOMContentLoaded', function () {
     var context = canvas.getContext('2d');
     var snap = document.getElementById('snap');
 
-    // Zatraži pristup kameri
+    // Definiranje ograničenja za korištenje zadnje kamere
+    var constraints = {
+        video: {
+            facingMode: "environment" // Traži korištenje zadnje kamere
+        }
+    };
+
+    // Zatraži pristup kameri s definiranim ograničenjima
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+            // Postavi izvor video elementa na dobiveni stream
             video.srcObject = stream;
             video.play();
+        })
+        .catch(function(error) {
+            console.log("Greška prilikom pristupa kameri: ", error);
         });
     }
 
     // Dodaj događaj na gumb za snimanje fotografije
     snap.addEventListener("click", function() {
+        // Nacrtaj trenutni prikaz video elementa na canvas element
         context.drawImage(video, 0, 0, 640, 480);
         // Sada možeš spremiti sliku s canvasa ili je dodatno obraditi
     });
 });
+
